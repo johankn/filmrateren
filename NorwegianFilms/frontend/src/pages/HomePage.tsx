7b4571c3b76841762ee3f913abda1c4369c90284
johakn <johakn@stud.ntnu.no>
2023-10-04 12:12:09 +0200

    added autocomplete component from material UI

--- NorwegianFilms/frontend/src/pages/HomePage.tsx
+++ NorwegianFilms/frontend/src/pages/HomePage.tsx
@@ -1,6 +1,24 @@
+import Autocomplete from "@mui/joy/Autocomplete";
+import Input from "@mui/joy/Input";
+import movieFile from "../../../backend/src/movies.json";
+import { useNavigate } from 'react-router-dom';
+
+
 function HomePage() {
-    return (<>HomePage</>
-    )
+  const navigate = useNavigate();
+
+
+  return (
+    <Autocomplete
+      options={movieFile.movies}
+      getOptionLabel={(option) => option.title}
+      onChange={(_event, newValue) => {
+        if (newValue) {
+          navigate(`/project2/moviePage/${newValue.id}`);
+        }
+      }}
+    />
+  );
 }
 
 export default HomePage;
