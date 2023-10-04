231772cab81d7a76d5b099139d6dbd10f663d044
elisemjoen <elismj@stud.ntnu.no>
2023-10-04 12:11:06 +0200

    Implemented MovieCard

--- NorwegianFilms/frontend/src/components/MovieCard.tsx
+++ NorwegianFilms/frontend/src/components/MovieCard.tsx
@@ -1,10 +1,12 @@
+import "./MovieCard.css"
+
 function MovieCard() {
     type Movie = {
         id: string;
         title: string;
         picture: string; //TODO: find out how to get picture
         description: string;
-        rating: number;
+        rating: number; //TODO: make RatingStars, issue #14
         genre: string;
         producer: string; 
         director: string;
@@ -25,17 +27,27 @@ function MovieCard() {
     
     return (
     <div className="movieCard">
-        <h1>{movie.title}</h1>
-        <div className="moviePicAndInfo">
-            <img src={movie.picture} alt="" />
-            <div className="movieInfo">
-                <p>Description: {movie.description}</p>
-                <p>Rating: {movie.rating}</p>
-                <p>Genre: {movie.genre}</p>
-                <p>Producer: {movie.producer}</p>
-                <p>Director: {movie.director}</p>
-            </div>
+        <div className="title">
+            <h1>{movie.title}</h1>
         </div>
+        <div className="picture">
+            <p>Insert picture here</p>
+        </div>  
+        <div className="info">
+            <div className="keyinfo">
+                <p>Sjanger: {movie.genre}</p>
+                <p>Produsent: {movie.producer}</p>
+                <p>Regi: {movie.director}</p>
+            </div>
+            <div className="rating">
+                <p>Rating: {movie.rating}</p>
+                <p>Insert rating stars here</p>
+                <p>Insert rating button here</p>
+            </div>
+            <div className="description">
+                <p>Description: {movie.description}</p>
+            </div>
+        </div>          
     </div>
     )
 }
