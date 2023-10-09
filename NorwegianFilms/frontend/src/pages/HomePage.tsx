379ff6d6358674a1427a5bee8c1b377aed02d1ac
Kristoffer Wetterhus <kkw@Kristoffers-MacBook-Pro-2.local>
2023-10-09 18:10:41 +0200

    Latest updates in homepage

--- NorwegianFilms/frontend/src/pages/HomePage.tsx
+++ NorwegianFilms/frontend/src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import "./HomePage.css";
 import screen from "../assets/screenContent.svg";
 import Autocomplete from "@mui/joy/Autocomplete";
@@ -11,21 +12,38 @@ function HomePage() {
     <div className="homePageStyle">
       <div className="screen">
         <img src={screen} alt="screenContent" />
+        <Autocomplete
+          className="searchBar"
+          options={movieFile.movies}
+          getOptionLabel={(option) => option.title}
+          onChange={(_event, newValue) => {
+            if (newValue) {
+              navigate(`/project2/moviePage/${newValue.id}`);
+            }
+          }}
+        />
         <div className="btn">
           <button onClick={() => (window.location.href = "./searchPage")}>
             Søk
           </button>
         </div>
       </div>
-      <Autocomplete
-        options={movieFile.movies}
-        getOptionLabel={(option) => option.title}
-        onChange={(_event, newValue) => {
-          if (newValue) {
-            navigate(`/project2/moviePage/${newValue.id}`);
-          }
-        }}
-      />
+      <div className="searchContainer">
+        <div className="extendedSearchBar">
+          <Autocomplete
+            options={movieFile.movies}
+            getOptionLabel={(option) => option.title}
+            onChange={(_event, newValue) => {
+              if (newValue) {
+                navigate(`/project2/moviePage/${newValue.id}`);
+              }
+            }}
+          />
+          <button onClick={() => (window.location.href = "./searchPage")}>
+            Søk
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
