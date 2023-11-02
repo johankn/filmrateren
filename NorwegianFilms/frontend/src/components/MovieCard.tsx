edbe6106ae398f1759a3021afc6b04602dc0011e
Rebekka Stangvik <rebekast@stud.ntnu.no>
2023-11-02 10:57:27 +0100

    last finish

--- NorwegianFilms/frontend/src/components/MovieCard.tsx
+++ NorwegianFilms/frontend/src/components/MovieCard.tsx
@@ -18,16 +18,16 @@ function MovieCard({ movie }: MovieCardProps) {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-2/3 mb-4   ">
-        <div className="mt-6 flex md:  justify-center md:justify-end max-w-full  ">
+        <div className="mt-6 flex justify-center md:justify-end max-w-full  ">
           <img src={movie.posterUrl === 'https://image.tmdb.org/t/p/w500None' ? noPoster : movie.posterUrl} alt={movie.title} className="w-3/4 md:w-96 h-auto "/>
         </div>
-        <div className="mt-6  info text-xsmall sm:text-small md:text-small lg:text-base pl-4 ">
+        <div className="mt-8  info text-xsmall sm:text-small md:text-small lg:text-base pl-4 ">
           <div className="mb-5">
             <p className="mb-2">
-              <span className="font-bold">Sjanger: </span> {movie.genres}
+              <span className="font-bold">Sjanger: </span> {movie.genres.join(', ')}
             </p>
             <p className="mb-2">
-              <span className="font-bold">Regi: </span> {movie.directors}
+              <span className="font-bold">Regi: </span> {movie.directors.join(', ')}
             </p>
             <p className="mb-2">
               <span className="font-bold">Utgivelsesår: </span> {movie.releaseYear}
