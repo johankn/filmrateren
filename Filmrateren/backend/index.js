96f70b490880be0b54f8f41af98c29811dcb1ba7
johakn <johakn@stud.ntnu.no>
2023-11-07 17:19:50 +0100

    changed corsOption to allow every domain

--- Filmrateren/backend/index.js
+++ Filmrateren/backend/index.js
@@ -16,7 +16,7 @@ const typeDefs = require('./src/graphql/typeDefs');
 const resolvers = require('./src/graphql/resolvers');
 
 const corsOptions = {
-  origin: 'http://it2810-05.idi.ntnu.no',
+  origin: '*',
   // origin: 'http://localhost:5173',
   // origin: 'https://studio.apollographql.com', 
   credentials: true  // allows cookies to be sent with the request
