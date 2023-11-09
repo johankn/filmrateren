f9111065a5a3c0b454e77d7d2513f1b458d0d9fc
Rebekka Stangvik <rebekast@stud.ntnu.no>
2023-11-09 12:11:58 +0100

    added react text to speech for movies

--- Filmrateren/frontend/src/components/MovieCard.tsx
+++ Filmrateren/frontend/src/components/MovieCard.tsx
@@ -1,6 +1,11 @@
 import Stars from './Stars';
 import noPoster from '../assets/noImage.png';
 import { Movie } from './types';
+import Speech from 'react-text-to-speech';
+import { AiFillSound } from 'react-icons/ai';
+import { FaVolumeMute } from 'react-icons/fa';
+import { IoMdRefresh } from 'react-icons/io';
+
 
 type MovieCardProps = {
   movie: Movie;
@@ -10,9 +15,19 @@ function MovieCard({ movie }: MovieCardProps) {
   const totalUserRatings = movie.userRatings.reduce((acc, curr) => acc + curr.rating, 0);
 
   const averageUserRating = movie.userRatings.length > 0 ? totalUserRatings / movie.userRatings.length : 0;
+  
+  const startBtn = <button className='text-large transform hover:scale-125 transition-transform'><AiFillSound></AiFillSound></button>
+  const pauseBtn = <button className='text-large transform hover:scale-125 transition-transform'><FaVolumeMute></FaVolumeMute></button>
+  const stopBtn = <button className='text-large transform hover:scale-125 transition-transform'><IoMdRefresh></IoMdRefresh></button>
 
-  return (
-    <div className="grid max-h-full gap-7 mt-6 ml-2 text-white italic place-items-center  ">
+return (
+  <div className="grid max-h-full gap-7 mt-6 ml-2 text-white italic place-items-center  ">
+    <Speech text={`${movie.title}. Sjanger er ${movie.genres.length < 1 ? 'Ukjent' : movie.genres.join(', ')}. Regi av: ${movie.directors.length < 1 ? 'Ukjent' : movie.directors.join(', ')}. Utgivelsesår er ${movie.releaseYear == 'Unknown' ? 'Ukjent' : movie.releaseYear}. Beskrivelse av filmen: ${movie.plot == '' ? 'Finner ingen beskrivelse' : movie.plot}.IMBD rating: ${movie.IMDBrating == 0 ? 'Ingen anmeldelser' : movie.IMDBrating}. Brukeranmeldelser: ${movie.userRatings.length < 1 ? 'Ingen anmeldelser' : averageUserRating.toFixed(1)} `}
+      rate={0.8}
+      startBtn={startBtn}
+      pauseBtn={pauseBtn}
+      stopBtn={stopBtn}
+      onError={() => console.error('Browser not supported!')}/>
       <div className="text-base sm:text-medium md:text-large lg:text-xl ">
         <h1>{movie.title}</h1>
       </div>
